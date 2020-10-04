0b015522c482b62157ee2f96d05b04af942ca464
myeongjilee <47519095+myeongjilee@users.noreply.github.com>
2020-10-05 02:05:35 +0900

    Update common.js

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-  /* back button */
-  function goBack() {
-    window.history.back();
-  }
+/* back button */
+function goBack() {
+  window.history.back();
+}
 
+$(document).ready(function(){
   /* touch event */
   $('.btn_link').on({
     'touchstart' : function () {
